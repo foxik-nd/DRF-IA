b558debaf4a17c63dc793a2c7a8ec8915f8bccb6
foxik-nd <froxik77430@outlook.fr>
2024-08-27 14:11:15 +0200

    CORRECTION

--- sentiment_analysis/sentiment/urls.py
+++ sentiment_analysis/sentiment/urls.py
@@ -3,5 +3,5 @@ from .views import TextAnalysisListCreate, TextClassificationListCreate
  
 urlpatterns = [
     path('analyses/', TextAnalysisListCreate.as_view(), name='analysis-list-create'),
-     path('classifications/', TextClassificationListCreate.as_view(), name='classification-list-create'),
+    path('classifications/', TextClassificationListCreate.as_view(), name='classification-list-create'),
 ]
